d951feb34a0fc0b465988cf02efbb7a7d46a3541
Cristian Bravo <cristian.bravog@uanl.edu.mx>
2025-08-16 22:28:30 -0600

    Add files via upload

---
+++ Ejercicio 4/auditoria_usuarios.ps1
@@ -0,0 +1,32 @@
+﻿#Script de auditoría básica de usuarios - Cristian Bravo
+
+$usuarios = Get-LocalUser
+$sinLogon = @()
+$conLogon = @()
+
+ForEach($u in $usuarios){
+
+    if(-not $u.LastLogon){
+
+        $sinLogon += "$($u.Name): Estado = $($u.Enabled), Último acceso = NUNCA"
+
+    } else {
+
+        $conLogon += "$($u.Name): Estado = $($u.Enabled), Último acceso = $($u.LastLogon)"
+    }
+}
+
+$sinLogon | Out-File -FilePath "C:\Users\Cris\Desktop\Practicas\sin_logon.txt"
+$conLogon | Out-File -FilePath "C:\Users\Cris\Desktop\Practicas\con_logon.txt"
+
+Write-Output " "
+Write-Output "------------------------- Auditoria -------------------------"
+Write-Output " "
+Write-Output " + Usuarios que NUNCA han iniciado sesión:"
+Write-Output " "
+$sinLogon | ForEach-Object {Write-Output "`t - $($_)"}
+
+Write-Output " "
+Write-Output " + Usuarios que SÍ han iniciado sesión:"
+Write-Output " "
+$conLogon | ForEach-Object {Write-Output "`t - $($_)"}
